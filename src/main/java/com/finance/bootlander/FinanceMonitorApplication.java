445f873ec5e5aaf64a5031a0296ec2b5dcc64721
Maciej <maciej.setorekgmail.com>
2023-02-27 20:55:46 +0100

    create project

---
+++ src/main/java/com/finance/bootlander/FinanceMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.finance.bootlander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceMonitorApplication.class, args);
+	}
+
+}
