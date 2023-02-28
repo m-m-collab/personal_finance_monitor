536fcdb9a8666b8cac69ae6f2b5fe94f062002c1
Maciej <maciej.setorek@gmail.com>
2023-03-01 00:45:37 +0100

    Add expense and income types

---
+++ src/main/java/com/finance/dto/IncomeType.java
@@ -0,0 +1,18 @@
+package com.finance.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Types of financial income.
+ */
+public enum IncomeType {
+
+    @JsonProperty("salary")
+    SALARY,
+
+    @JsonProperty("other")
+    OTHER,
+
+    //TODO: add more types.
+
+}
